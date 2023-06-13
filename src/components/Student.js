216b0f343d6afab413b52d9885634732d4c5961d
Airis Wang <airis.wang2014@fgvmail.br>
2023-06-12 22:24:28 -0400

    implemented nested components

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Student = () => {
+const Student = (props) => {
     return (
         <ul>
-            <li>Nickname: Ada</li>
-            <li>Email: ada@dev.gov</li>
+            <li>Nickname: {props.name}</li>
+            <li>Email: {props.email}</li>
         </ul>
     )
 }
--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -2,17 +2,21 @@ import React from 'react';
 import './StudentList.css';
 import Student from './Student';
 
-const StudentList = () => {
+const StudentList = (props) => {
     const headingClass = 'student-list__heading';
     const listClass = 'student-list';
+    
+    const studentComponents = props.students.map(student => {
+        return(
+            <li><Student name={student.name} email={student.email}></Student></li>
+        );
+    });
 
     return (
         <section>
             <h2 className={headingClass}>Student List</h2>
             <ul className={listClass}>
-                <li><Student /></li>
-                <li><Student /></li>
-                <li><Student /></li>
+                {studentComponents}
             </ul>
         </section>
     );
